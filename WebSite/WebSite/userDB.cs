433acde3ad552b79458e2f91b1bc5e0b68848a91
Andreas Granfält <andreas.granfalt@gmail.com>
2014-04-10 11:27:58 +0200

    Email check

--- WebSite/WebSite/userDB.cs
+++ WebSite/WebSite/userDB.cs
@@ -14,20 +14,50 @@ namespace WebSite
 
         public void RegUser(String email, String password, String firstName, String lastName, String phoneNumber)
         {
-            using(SqlConnection _conn = new SqlConnection(ConnectionString))
-            using(SqlCommand _cmd = new SqlCommand("dbo.Usp_RegisterUser", _conn))
+            if(!UserEmailExists(email))
+            {
+                using(SqlConnection _conn = new SqlConnection(ConnectionString))
+                using(SqlCommand _cmd = new SqlCommand("dbo.Usp_RegisterUser", _conn))
+                {
+                    _cmd.CommandType = CommandType.StoredProcedure;
+                    _cmd.Parameters.Add(new SqlParameter("@Type", "User"));
+                    _cmd.Parameters.Add(new SqlParameter("@Email", email));
+                    _cmd.Parameters.Add(new SqlParameter("@Password", password));
+                    _cmd.Parameters.Add(new SqlParameter("@FirstName", firstName));
+                    _cmd.Parameters.Add(new SqlParameter("@LastName", lastName));
+                    _cmd.Parameters.Add(new SqlParameter("@PhoneNumber", phoneNumber));
+
+                    _conn.Open();
+                    _cmd.ExecuteNonQuery();
+                    _conn.Close();
+                }
+            }
+            else
+            {
+            }
+        }
+
+        public Boolean UserEmailExists(String email)
+        {
+            using (SqlConnection _conn = new SqlConnection(ConnectionString))
+            using (SqlCommand _cmd = new SqlCommand("dbo.Usp_SelectUserEmail", _conn))
             {
                 _cmd.CommandType = CommandType.StoredProcedure;
-                _cmd.Parameters.Add(new SqlParameter("@Type", "User"));
                 _cmd.Parameters.Add(new SqlParameter("@Email", email));
-                _cmd.Parameters.Add(new SqlParameter("@Password", password));
-                _cmd.Parameters.Add(new SqlParameter("@FirstName", firstName));
-                _cmd.Parameters.Add(new SqlParameter("@LastName", lastName));
-                _cmd.Parameters.Add(new SqlParameter("@PhoneNumber", phoneNumber));
+
+                SqlParameter emailCount = new SqlParameter("@EmailCount", SqlDbType.Int);
+                emailCount.Direction = ParameterDirection.Output;
+
+                _cmd.Parameters.Add(emailCount);
 
                 _conn.Open();
                 _cmd.ExecuteNonQuery();
                 _conn.Close();
+
+                if (Convert.ToInt32(emailCount.Value) == 0)
+                    return false;
+                else
+                    return true;
             }
         }
     }
