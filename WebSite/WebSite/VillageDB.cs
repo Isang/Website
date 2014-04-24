7551852874bc7ad0402495e78069ef8bd0ac2a13
JoakimCarlsson <Joakimcarlsson1994@gmail.com>
2014-04-24 11:14:24 +0200

    Testing

--- WebSite/WebSite/VillageDB.cs
+++ WebSite/WebSite/VillageDB.cs
@@ -24,6 +24,7 @@ namespace WebSite
 
                 SqlDataAdapter adr = new SqlDataAdapter(_cmd);
                 adr.Fill(table);
+                //slurp
             }
             return table;
            
